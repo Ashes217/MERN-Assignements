8876981080f10eb919fd78f6ab20e7eb754f1c4c
Ashes217 <astrickland21714@gmail.com>
2022-12-17 11:29:19 -0600

    Author assignment

---
+++ Authors/server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+//connect server to database
+require('./config/mongoose.config')
+
+//Middle ware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}))
+
+const Routes = require('./routes/author.routes')
+Routes(app)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
